9cba86e06ff83ab2b11413324a6070d222fe3b31
gaoyukun <1913933460@qq.com>
2023-03-04 21:00:07 +0800

    run on mac for the first time

---
+++ Framework/src/Rendering/CubismRenderer.cpp
@@ -0,0 +1,145 @@
+﻿/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+#include "CubismRenderer.hpp"
+#include "CubismFramework.hpp"
+#include "Model/CubismModel.hpp"
+
+//------------ LIVE2D NAMESPACE ------------
+namespace Live2D { namespace Cubism { namespace Framework { namespace Rendering {
+
+void CubismRenderer::Delete(CubismRenderer* renderer)
+{
+    CSM_DELETE_SELF(CubismRenderer, renderer);
+}
+
+CubismRenderer::CubismRenderer()
+    : _isCulling(false)
+    , _isPremultipliedAlpha(false)
+    , _anisotropy(0.0f)
+    , _model(NULL)
+    , _useHighPrecisionMask(false)
+{
+    //単位行列に初期化
+    _mvpMatrix4x4.LoadIdentity();
+}
+
+CubismRenderer::~CubismRenderer()
+{}
+
+void CubismRenderer::Initialize(Framework::CubismModel* model)
+{
+    Initialize(model, 1);
+}
+
+void CubismRenderer::Initialize(Framework::CubismModel* model, csmInt32 maskBufferCount)
+{
+    _model = model;
+}
+
+void CubismRenderer::DrawModel()
+{
+    if (GetModel() == NULL) return;
+
+    /**
+     * 在绘制DoDrawModel之前和绘制之后，请调用以下函数：。。
+     * ・SaveProfile();
+     * ・RestoreProfile();
+     * 这是通过保存和恢复渲染器绘图设置
+     * 用于还原到模型绘制之前的状态的过程。
+     */
+
+    SaveProfile();
+
+    DoDrawModel();
+
+    RestoreProfile();
+}
+
+void CubismRenderer::SetMvpMatrix(CubismMatrix44* matrix4x4)
+{
+    _mvpMatrix4x4.SetMatrix(matrix4x4->GetArray());
+}
+
+CubismMatrix44 CubismRenderer::GetMvpMatrix() const
+{
+    return _mvpMatrix4x4;
+}
+
+void CubismRenderer::SetModelColor(csmFloat32 red, csmFloat32 green, csmFloat32 blue, csmFloat32 alpha)
+{
+    if (red < 0.0f) red = 0.0f;
+    else if (red > 1.0f) red = 1.0f;
+
+    if (green < 0.0f) green = 0.0f;
+    else if (green > 1.0f) green = 1.0f;
+
+    if (blue < 0.0f) blue = 0.0f;
+    else if (blue > 1.0f) blue = 1.0f;
+
+    if (alpha < 0.0f) alpha = 0.0f;
+    else if (alpha > 1.0f) alpha = 1.0f;
+
+    _modelColor.R = red;
+    _modelColor.G = green;
+    _modelColor.B = blue;
+    _modelColor.A = alpha;
+}
+
+CubismRenderer::CubismTextureColor CubismRenderer::GetModelColor() const
+{
+    return  _modelColor;
+}
+
+void CubismRenderer::IsPremultipliedAlpha(csmBool enable)
+{
+    _isPremultipliedAlpha = enable;
+}
+
+csmBool CubismRenderer::IsPremultipliedAlpha() const
+{
+    return _isPremultipliedAlpha;
+}
+
+void CubismRenderer::IsCulling(csmBool culling)
+{
+    _isCulling = culling;
+}
+
+csmBool CubismRenderer::IsCulling() const
+{
+    return  _isCulling;
+}
+
+void CubismRenderer::SetAnisotropy(csmFloat32 n)
+{
+    _anisotropy = n;
+}
+
+csmFloat32 CubismRenderer::GetAnisotropy() const
+{
+    return _anisotropy;
+}
+
+CubismModel* CubismRenderer::GetModel() const
+{
+    return _model;
+}
+
+void CubismRenderer::UseHighPrecisionMask(csmBool high)
+{
+    _useHighPrecisionMask = high;
+}
+
+csmBool CubismRenderer::IsUsingHighPrecisionMask()
+{
+    return _useHighPrecisionMask;
+}
+
+}}}}
+
+//------------ LIVE2D NAMESPACE ------------
---
+++ Framework/src/Type/csmString.hpp
@@ -0,0 +1,240 @@
+﻿/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+#pragma once
+
+#include "CubismFramework.hpp"
+#include <string.h>
+
+//--------- LIVE2D NAMESPACE ------------
+namespace Live2D { namespace Cubism { namespace Framework {
+
+/**
+ * @brief   字符串类<br>
+ *           为了避免在消费者游戏机等中嵌入STL的安装。<br>
+ *           std::string 的简易版多字节字符串未对应
+ */
+class csmString
+{
+public:
+    /**
+     * @brief   构造函数
+     *
+     */
+    csmString();
+
+    /**
+     * @brief   参数构造函数
+     *
+     * @param   c   ->  コピーする文字列
+     */
+    csmString(const csmChar* c);
+
+    /**
+     * @brief   引数付きコンストラクタ
+     *
+     * @param   c       ->  文字列
+     * @param   length  ->  文字列からコピーする長さ
+     */
+    csmString(const csmChar* c, csmInt32 length);
+
+    /**
+     * @brief   引数付きコンストラクタ
+     *
+     * @param[in]   s   ->  文字列
+     */
+    csmString(const csmString& s);
+
+    /**
+     * @brief   引数付きコンストラクタ
+     *
+     * @param[in]   c       ->  文字列
+     * @param[in]   length  ->  文字列からコピーする長さ
+     * @param[in]   usePtr  ->  trueなら引数のポインタを保持する（インスタンスと共に破棄される）<br> falseなら内部でコピーを実行する
+     */
+    csmString(const csmChar* c, csmInt32 length, csmBool usePtr);
+
+    /**
+     * @brief   デストラクタ
+     *
+     */
+    virtual ~csmString();
+
+    /**
+     * @brief =演算子のオーバーロード(csmString型)
+     */
+    csmString& operator=(const csmString& s);
+
+    /**
+     * @brief =演算子のオーバーロード(csmChar型)
+     */
+    csmString& operator=(const csmChar* c);
+
+    /**
+     * @brief ==演算子のオーバーロード(csmString型)
+     */
+    csmBool operator==(const csmString& s) const;
+
+    /**
+     * @brief ==演算子のオーバーロード(csmChar型)
+     */
+    csmBool operator==(const csmChar* c) const;
+
+    /**
+     * @brief <演算子のオーバーロード(csmString型)
+     */
+    csmBool operator<(const csmString& s) const;
+
+    /**
+     * @brief <演算子のオーバーロード(csmChar型)
+     */
+    csmBool operator<(const csmChar* c) const;
+
+    /**
+     * @brief >演算子のオーバーロード(csmString型)
+     */
+    csmBool operator>(const csmString& s) const;
+
+    /**
+     * @brief >演算子のオーバーロード(csmChar型)
+     */
+    csmBool operator>(const csmChar* c) const;
+
+    /**
+     * @brief +演算子のオーバーロード(csmString型)
+     */
+    csmString operator+(const csmString& s) const;
+
+    /**
+     * @brief +演算子のオーバーロード(csmChar型)
+     */
+    csmString operator+(const csmChar* c) const;
+
+    /**
+     * @brief +=演算子のオーバーロード(csmString型)
+     */
+    csmString& operator+=(const csmString& s);
+
+    /**
+     * @brief +=演算子のオーバーロード(csmChar型)
+     */
+    csmString& operator+=(const csmChar* c);
+
+    /**
+     * @brief  文字列を後方に追加する
+     *
+     * @param[in]   c   ->  文字列
+     * @param[in]   length  ->  追加する文字列の長さ
+     * @return 更新された文字列
+     */
+    csmString& Append(const csmChar* c, csmInt32 length);
+
+    /**
+     * @brief   文字サイズを拡張して文字を埋める
+     *
+     * @param[in]   length   ->  拡張する文字数
+     * @param[in]   v        ->  埋める文字
+     * @return 更新された文字列
+     */
+    csmString& Append(csmInt32 length, const csmChar v);
+
+    /**
+     * @brief   文字列の長さを返す
+     *
+     * @return  文字列の長さ
+     */
+    csmInt32 GetLength() const { return _length; }
+
+    /**
+     * @brief   C言語文字列としてのポインタを取得する
+     *
+     * @return   C言語文字列のポインタ
+     */
+    const csmChar* GetRawString() const;
+
+    /**
+     * @brief   ポインタを解放する
+     */
+    void Clear();
+
+    /**
+     * @brief   ハッシュコードを取得する
+     *
+     * @return  ハッシュコード
+     */
+    csmInt32 GetHashcode();
+
+
+protected:
+
+    /**
+     * @brief   文字列をコピーする. 終端に\0を追加する
+     *
+     * @param[in]   c   ->  文字列
+     * @param[in]   length  ->  コピーする文字数
+     */
+    void Copy(const csmChar* c, csmInt32 length);
+
+    /**
+     * @brief   csmStringインスタンスの初期化関数。文字列のセットとハッシュコードの算出を行う。
+     *
+     * @param[in]   c       ->  文字列のポインタ
+     * @param[in]   length  ->  文字列の長さ
+     * @param[in]   usePtr  ->  trueなら第一引数のポインタを保持する(インスタンスと共に破棄される)。<br> falseなら内部でコピーを実行する
+     */
+    void Initialize(const csmChar* c, csmInt32 length, csmBool usePtr);
+
+    /**
+     * @brief   文字列からハッシュ値を生成して返す
+     *
+     * @param[in]   c       ->  文字列
+     * @param[in]   length  ->  ハッシュ値の長さ
+     * @return      文字列から生成したハッシュ値
+     */
+    csmInt32 CalcHashcode(const csmChar* c, csmInt32 length);
+
+private:
+    static const csmInt32 SmallLength = 64; ///< この長さ-1未満の文字列は内部バッファを使用
+    static const csmInt32 DefaultSize = 10; ///< デフォルトの文字数
+    static csmInt32 s_totalInstanceNo;      ///< 通算のインスタンス番号
+    csmChar* _ptr;                          ///< 字符数组指针
+    csmInt32 _length;                       ///< 半角字符数（内存确保最后为0，因此_length+1）
+    csmInt32 _hashcode;                     ///< 实例的散列值
+    csmInt32 _instanceNo;                   ///< 分配给实例的编号
+
+    csmChar _small[SmallLength];            ///< 如果字符串长度小于SmallLength-1，请使用此处
+
+    /**
+     * @brief 文字列が空かどうか？
+     *
+     * 文字列が空かどうか確認する。
+     *
+     * @retval  true    空の文字列
+     * @retval  false   値が設定されている
+     */
+    csmBool IsEmpty() const;
+
+    /**
+     * @brief 文字列を空に設定
+     *
+     * 文字列を空に設定する。
+     *
+     * @note ポインタに対してfree()はしない。
+     */
+    void SetEmpty();
+
+    /**
+     * @brief   C言語文字列としてのポインタを取得する
+     *
+     * @return   C言語文字列のポインタ
+     */
+    csmChar* WritePointer();
+};
+}}}
+
+//------------------------- LIVE2D NAMESPACE -----------
+
